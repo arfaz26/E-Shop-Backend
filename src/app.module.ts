0a1dd2bf271d8a31e14c1fb7f0bb0f8287d518fd
arfaz26 <chougulearfaz@gmail.com>
2022-08-07 11:23:49 +0530

    add env configuration service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigurationService } from 'shared/configuration/configuration.service';
+import { SharedModule } from 'shared/shared.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/e-store')],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV
+        ? `.env.${process.env.NODE_ENV}`
+        : '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigurationService],
+      useFactory: async () => ({
+        uri: ConfigurationService.mongoUrl,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+    }),
+    SharedModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
