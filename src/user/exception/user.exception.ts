c27d0664499d65a03c3d5899a3a9c309b3080bfc
arfaz26 <chougulearfaz@gmail.com>
2022-08-11 21:16:39 +0530

    add user login api

--- src/user/exception/user.exception.ts
+++ src/user/exception/user.exception.ts
@@ -1,9 +1,22 @@
 import { HttpStatus } from '@nestjs/common';
 import { ERROR_CODES } from 'shared/enums/error-code.enum';
+import { NotFoundError } from 'shared/exceptions/not-found.exception';
 import { ValidationError } from 'shared/exceptions/validation.exception';
 
 export class EmailAlreadyExistsError extends ValidationError {
   constructor() {
     super(ERROR_CODES.EMAIL_ALREADY_EXISTS, HttpStatus.CONFLICT);
+  }
+}
+
+export class UserNotFoundError extends NotFoundError {
+  constructor() {
+    super(ERROR_CODES.USER_NOT_FOUND);
+  }
+}
+
+export class InvalidCredentialsError extends ValidationError {
+  constructor() {
+    super(ERROR_CODES.INVALID_CREDENTIALS, HttpStatus.UNAUTHORIZED);
   }
 }
--- src/user/repository/user.repository.ts
+++ src/user/repository/user.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { BaseRepository, ModelType } from 'shared/repository/base.repository';
+import { UserEntity } from 'user/entity/user.entity';
 import User from 'user/schema/user.schema';
 
 @Injectable()
@@ -9,5 +10,10 @@ export class UserRepository extends BaseRepository<User> {
     @InjectModel(User.modelName) private readonly userModel: ModelType<User>,
   ) {
     super(userModel);
+  }
+
+  async findByEmail(email: string): Promise<UserEntity> {
+    const user = await this.findOne({ email });
+    return user ? UserEntity.toEntity(user) : null;
   }
 }
