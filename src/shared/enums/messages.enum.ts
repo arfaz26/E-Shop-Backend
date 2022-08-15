e7164b3a45141a137cf5d42f926979ae29a20a10
arfaz26 <chougulearfaz@gmail.com>
2022-08-15 21:29:43 +0530

    add reset password api

--- src/shared/enums/messages.enum.ts
+++ src/shared/enums/messages.enum.ts
@@ -5,9 +5,11 @@ export enum MESSAGES {
   UNAUTHORIZED = 'You are not Authorized to access this resource',
   NEW_PASSWORD_MISMATCH = 'New password and confirm password mismatch',
   INVALID_PASSWORD = 'Password is incorrect',
+  RESET_PASSWORD_LINK_EXPIRED = 'Reset password link has expired',
 }
 
 export enum SUCCESS_MESSAGES {
   FORGOT_PASSWORD = 'Link to reset your password is sent to your registered email',
   UPDATE_PASSWORD_SUCCESSFUL = 'Password has been changed successfully',
+  RESET_PASSWORD_SUCCESSFUL = 'Password has been changed successfully',
 }
